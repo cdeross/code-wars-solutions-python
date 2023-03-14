d053155dd6f79ad2bbdd09c614d8a38d85b14ecd
cdeross <127800329+cdeross@users.noreply.github.com>
2023-03-13 21:15:13 -0700

    the power of two

--- 7 kyu the power of two.py
+++ 7 kyu the power of two.py
@@ -1,5 +1,5 @@
 """
-7kyu - the power of two 
+7kyu - the power of two
 
 Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language) that determines if a given non-negative integer is a power of two. From the corresponding Wikipedia entry:
 a power of two is a number of the form 2n where n is an integer, i.e. the result of exponentiation with number two as the base and integer n as the exponent.
